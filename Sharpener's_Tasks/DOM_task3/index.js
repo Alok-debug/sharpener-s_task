0f20947fb34b76addb575da6e61d306dd47a4c1f
Alok Kumar <75585339+Alok-debug@users.noreply.github.com>
2022-10-01 16:46:00 +0530

    added green color and font weight

--- Sharpener's_Tasks/DOM_task3/index.js
+++ Sharpener's_Tasks/DOM_task3/index.js
@@ -10,4 +10,13 @@ var items = document.getElementsByClassName('list-group-item');
 for (let i = 0; i < items.length;i++){
     items[i].style.color = "brown";
     items[i].style.fontWeight = "bolder";
-    items[i].style.background="grey"}
+    items[i].style.background = "grey"
+    if (i == 2) {
+        items[i].style.background='green'
+    }
+    if (i == 3) {
+        items[i].style.background = 'red';
+        items[i].innerHTML = '<h1>Hey</h1>';
+        items[i].style.color='blue'
+    }
+}
