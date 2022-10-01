94426f58051d794fa7130b8a48389c858a1543c3
Alok Kumar <75585339+Alok-debug@users.noreply.github.com>
2022-10-01 18:22:01 +0530

    task done--add green to 2nd

--- Sharpener's_Tasks/DOM_task3/index.js
+++ Sharpener's_Tasks/DOM_task3/index.js
@@ -6,20 +6,21 @@
 // headerTitle.style.borderBottom='solid  red'
 
 //------ GET ELEMENTs BY CLASS-NAME-----(gives groups of element with same class Name)
-// var items = document.getElementsByClassName('list-group-item');
-// for (let i = 0; i < items.length;i++){
-//     items[i].style.color = "brown";
-//     items[i].style.fontWeight = "bolder";
-//     items[i].style.background = "grey"
-//     if (i == 2) {
-//         items[i].style.background='green'
-//     }
-//     if (i == 3) {
-//         items[i].style.background = 'red';
-//         items[i].innerHTML = '<h1>Hey</h1>';
-//         items[i].style.color='blue'
-//     }
-// }
+var items = document.getElementsByClassName('list-group-item');
+for (let i = 0; i < items.length;i++){
+    items[i].style.color = "brown";
+    items[i].style.fontWeight = "bolder";
+    items[i].style.background = "grey"
+    if (i == 1) {
+        items[i].style.background='green'
+    }
+    if (i == 2) {
+        items[i].style.background = 'red';
+        items[i].innerHTML = '<h1>Hey</h1>';
+        items[i].style.color = 'blue'
+        items[i].style.display='none'
+    }
+}
 
 //-------------get elements by tag Name-----
 
@@ -73,15 +74,15 @@
 // ------------querySelectorAll()---------------------------//
 
 
-var titles = document.querySelectorAll('.title');
-console.log(titles)
-titles[0].innerHTML = '<h1>querySelectorAll</h1>';
+// var titles = document.querySelectorAll('.title');
+// console.log(titles)
+// titles[0].innerHTML = '<h1>querySelectorAll</h1>';
 
-var odd = document.querySelectorAll('li:nth-child(odd)');
-var even = document.querySelectorAll('li:nth-child(even)');
+// var odd = document.querySelectorAll('li:nth-child(odd)');
+// var even = document.querySelectorAll('li:nth-child(even)');
 
-for (let i = 0; i < odd.length; i++){
-    odd[i].style.background = 'yellow';
-    even[i].style.background = 'blue';
+// for (let i = 0; i < odd.length; i++){
+//     odd[i].style.background = 'yellow';
+//     even[i].style.background = 'blue';
 
-}
+// }
