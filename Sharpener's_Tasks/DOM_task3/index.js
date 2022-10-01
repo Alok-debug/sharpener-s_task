b975e34fc77377260a869d36b599b890b952d9ed
Alok Kumar <75585339+Alok-debug@users.noreply.github.com>
2022-10-01 17:33:20 +0530

    tried difference btw elements by classNam and by Tage Name

--- Sharpener's_Tasks/DOM_task3/index.js
+++ Sharpener's_Tasks/DOM_task3/index.js
@@ -6,17 +6,35 @@ var headerTitle=document.getElementById('header-title')
 // headerTitle.style.borderBottom='solid  red'
 
 //------ GET ELEMENTs BY CLASS-NAME-----(gives groups of element with same class Name)
-var items = document.getElementsByClassName('list-group-item');
-for (let i = 0; i < items.length;i++){
-    items[i].style.color = "brown";
-    items[i].style.fontWeight = "bolder";
-    items[i].style.background = "grey"
+// var items = document.getElementsByClassName('list-group-item');
+// for (let i = 0; i < items.length;i++){
+//     items[i].style.color = "brown";
+//     items[i].style.fontWeight = "bolder";
+//     items[i].style.background = "grey"
+//     if (i == 2) {
+//         items[i].style.background='green'
+//     }
+//     if (i == 3) {
+//         items[i].style.background = 'red';
+//         items[i].innerHTML = '<h1>Hey</h1>';
+//         items[i].style.color='blue'
+//     }
+// }
+
+//-------------get elements by tag Name-----
+
+var liItems = document.getElementsByTagName('li');
+
+for (let i = 0; i <liItems.length;i++){
+    liItems[i].style.color = "brown";
+    liItems[i].style.fontWeight = "bolder";
+    liItems[i].style.background = "grey"
     if (i == 2) {
-        items[i].style.background='green'
+        liItems[i].style.background='green'
     }
     if (i == 3) {
-        items[i].style.background = 'red';
-        items[i].innerHTML = '<h1>Hey</h1>';
-        items[i].style.color='blue'
+        liItems[i].style.background = 'red';
+        liItems[i].innerHTML = '<h1>Hey</h1>';
+        liItems[i].style.color='blue'
     }
 }
