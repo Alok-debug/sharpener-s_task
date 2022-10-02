3cb027b348412c3412263928328311e2d7374964
Alok Kumar <75585339+Alok-debug@users.noreply.github.com>
2022-10-03 02:01:43 +0530

    added customer details in li using DOM

--- Sharpener's_Tasks/DOM_task10/index.js
+++ Sharpener's_Tasks/DOM_task10/index.js
@@ -3,6 +3,13 @@ form.addEventListener('submit', storeInput);
 var customerID = 1;
 var random = Math.random();
 
+
+// add ul to DOM
+var ul = document.createElement('ul');
+ul.id = 'show__details';
+var mainDiv = document.getElementById('main__comtainer');
+mainDiv.appendChild(ul);
+
 function storeInput(e) {
     e.preventDefault();
     customerID *= random;
@@ -12,8 +19,23 @@ function storeInput(e) {
         mobileNo: `${document.getElementById('contact1').value  }`,
         appointmentTime: `${document.getElementById('dateTime').value }`
     };
-
     console.log(customerData);
     var jsonCustomer = JSON.stringify(customerData);
-    localStorage.setItem(`${customerID}`,jsonCustomer);
+    localStorage.setItem(`${customerID}`, jsonCustomer);
+
+
+    var li = document.createElement('li');
+    var detailsText = document.createTextNode(`name: ${document.getElementById('personn').value}, MobileNo: ${document.getElementById('contact1').value}, ${document.getElementById('dateTime').value}`);
+    li.appendChild(detailsText);
+    ul.appendChild(li);
+    
+
+
 }
+
+
+
+
+
+
+
