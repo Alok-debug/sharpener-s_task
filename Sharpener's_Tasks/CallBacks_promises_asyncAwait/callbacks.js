a4f4f18a815a19c05dd1e934902c11e6820a662a
Alok Kumar <75585339+Alok-debug@users.noreply.github.com>
2022-10-07 20:11:33 +0530

    callbacks

--- Sharpener's_Tasks/CallBacks_promises_asyncAwait/callbacks.js
+++ Sharpener's_Tasks/CallBacks_promises_asyncAwait/callbacks.js
@@ -3,27 +3,60 @@ const posts = [
     {title:'Post Two', body:'This is post one'}
 ];
 
-let currTime = new Date().getTime();
-console.log(currTime);
+
 
 function getPosts() {
     setTimeout(() => {
         let output = '';
         posts.forEach((post, index) => {
-            output += `<li>${post.title}, post created:@ ${Math.floor((new Date().getTime()-currTime)/1000)} ,Seconds Ago</li>`;
+            output += `<li>${post.title}, post created:@ ${(new Date().toLocaleTimeString())} ,Seconds Ago</li>`;
         });
         document.body.innerHTML = output;
-    }, 1000);
+    },10000);
 }
 
-function createPost(post, callback) {
-    setTimeout(() => {
-        posts.push(post);
-        callback();
-    }, 2000);
+function createPost(callback) {
+    const postData = [
+        {title:'Post Three', body:'This is post three' },
+        { title: 'Post four', body: 'This is post four' },
+        {title:'Post fivr', body:'This is post fivr' },
+        { title: 'Post six', body: 'This is post six' },
+        {title:'Post seven', body:'This is post sevev' },
+        { title: 'Post eight', body: 'This is post eight' },
+        {title:'Post nine', body:'This is post nine' },
+        {title:'Post ten', body:'This is post ten'}
+    ];
+    for (let i = 0; i < postData.length; i++){
+        setTimeout(() => {
+        posts.push(postData[i]);
+            
+        }, i * 1000);
+        
+    }
+    callback();
+    // for (let i = 0; i < postData.length; i++){
+    //     setTimeout(() => {
+    //     posts.pop(postData[postData.length-1-i]);
+    //         callback();
+    //     }, i * 1000);
+        
+    // }
+    
     
 }
 
 
-createPost({ title: 'Post three', body: 'This is post three' }, getPosts)
-createPost({title:'Post four', body:'This is post four'},getPosts)
+createPost(getPosts);
+
+let currTime = new Date().getTime();
+console.log(currTime);
+
+// var li = document.getElementsByTagName('li');
+// setTimeout(() => {
+//     for (let i = 0; i < li.length; i++) {
+//         //create text node
+//         //var text = document.createTextNode();
+//         console.log(li[i].firstChild);
+//     }
+// },10000)
+
