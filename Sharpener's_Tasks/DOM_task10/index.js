674f38a9475004a328f3bbf20b7d19bdb42c1592
Alok Kumar <75585339+Alok-debug@users.noreply.github.com>
2022-10-09 20:35:37 +0530

    GET data from crud-crud and display the overscreen

--- Sharpener's_Tasks/DOM_task10/index.js
+++ Sharpener's_Tasks/DOM_task10/index.js
@@ -1,7 +1,7 @@
 var form = document.getElementById('inputf');
 form.addEventListener('submit', storeInput);
-var customerID = 1;
-var random = Math.random();
+var customerID = 0;
+
 
 
 // add ul to DOM
@@ -12,25 +12,34 @@ mainDiv.appendChild(ul);
 
 function storeInput(e) {
     e.preventDefault();
-    customerID *= random;
+    customerID += 1;
 
     var customerData = {
-        name: `${document.getElementById('personn').value }`,//form.children[1].value 
-        mobileNo: `${document.getElementById('contact1').value  }`,
-        appointmentTime: `${document.getElementById('dateTime').value }`
+        custId:`${document.getElementById('contact1').value}`,
+        name: `${document.getElementById('personn').value}`,//form.children[1].value 
+        mobileNo: `${document.getElementById('contact1').value}`,
+        appointmentTime: `${document.getElementById('dateTime').value}`
     };
-    console.log(customerData);
+    //console.log(customerData);
     // var jsonCustomer = JSON.stringify(customerData);
-    // localStorage.setItem(`${customerID}`, jsonCustomer);
+    // localStorage.setItem(`${customerData.mobileNo}`, jsonCustomer);
 
-// Instead of putting into local storage, Now try to put data on cloud using crud-crud, and axios 
+
+// // Instead of putting into local storage, Now try to put data on cloud using crud-crud, and axios
     axios.post('https://crudcrud.com/api/71ed7413c8ff47c6a113fb1dcb8919b7/appoinmentData', customerData)
         .then(res => console.log(res))
         .catch(err => console.log(err));
+// show on the screen
+    showNewUserOnScreen(customerData);
+}
 
+
+
+
+function showNewUserOnScreen(user){
     // show ul li items who have registered 
     var li = document.createElement('li');
-    var detailsText = document.createTextNode(`name: ${document.getElementById('personn').value}, MobileNo: ${document.getElementById('contact1').value}, ${document.getElementById('dateTime').value}`);
+    var detailsText = document.createTextNode(`name: ${user.name}, MobileNo: ${user.mobileNo}, ${user.appointmentTime}`);
     li.appendChild(detailsText);
     
     // create delete and edit button
@@ -50,7 +59,6 @@ function storeInput(e) {
     ul.appendChild(li);
 }
 
-
 // delete functiionality
 // element remove functionality
 var itemList = document.getElementById('show__details');
@@ -67,21 +75,25 @@ function removeItem(e) {
 }
 
 
-// delete functiionality
-// element remove functionality
-var itemList = document.getElementById('show__details');
-itemList.addEventListener('click', removeItem);
-function removeItem(e) {
-    //console.log(e);
-    if (e.target.classList.contains('edit')) {
-        var li = e.target.parentElement;
-        itemList.removeChild(li);
-        
-        
-    }
-}
+// when DOM Content gets loaded;
 
-
+window.addEventListener('DOMContentLoaded', () => {
+    axios.get('https://crudcrud.com/api/71ed7413c8ff47c6a113fb1dcb8919b7/appoinmentData')
+        .then(response => { return (response.data)})
+        .then(data => {
+            data.forEach(obj=>showNewUserOnScreen(obj))
+        })
+        .catch(err => console.log(err));
+    // const localstorageobj = localStorage;
+    // const localstoragekeys = Object.keys(localstorageobj);
+    // for (var i = 0; i < localstoragekeys.length; i++){
+    //     const key = localstoragekeys[i];
+    //     const userDetailString = localstorageobj[key];
+    //     const userDetailsObj = JSON.parse(userDetailString);
+    //     showNewUserOnScreen(userDetailsObj);
+    // }
+    
+})
 
 
 
